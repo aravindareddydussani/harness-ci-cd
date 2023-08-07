238bb68d0157368584d53a4ded625e19fbe30119
aravindareddydussani <aravinda-reddy.dussani@lloydsbanking.com>
2023-08-07 15:53:33 +0100

    feat/csdaai-600 added provider config

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0.0"
+    }
+  }
+
+}
+
+provider "google" {
+  project = var.project_id
+}
+
+provider "google-beta" {
+  project = var.project_id
+}
+
+variable "project_id" {
+  description = "The ID of the GCP project where resources will be deployed"
+  type        = string
+  default = "playpen-c77259"
+}
+
 #enable the cloud workstation api
 resource "google_project_service" "data_science_work_station_enable" {
   service = "workstations.googleapis.com"
@@ -8,3 +32,4 @@ resource "google_project_service" "data_science_work_station_enable" {
   disable_dependent_services = true
   disable_on_destroy         = true
 }
+
